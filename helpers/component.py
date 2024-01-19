efb3c45a2ab1ee041b9dafee784fa77a8a996b44
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-01-19 01:05:13 +0100

    fix: fixed type

--- helpers/component.py
+++ helpers/component.py
@@ -1,13 +1,22 @@
+from typing import Any
 from abc import ABC, abstractmethod
 from _thread import start_new_thread
 
+
 class ThreadedComponentClass(ABC):
     """
-    A abstract class for all components that should need a state to be set by the controller.
+    A helper class for creating threaded components.
 
-    This class is used to make sure that all components have a main_loop method and that they can be started in a thread.
+    Comes with a start_loop_thread method that starts the main_loop method in a new thread.
+
+    Args:
+        controller_class (DualshockInterface): The controller class to get the events from.
     
     """
+
+    def __init__(self, controller_class: Any) -> None:
+        self.controller = controller_class
+        self.run = False
 
     @abstractmethod
     def main_loop(self) -> None:
@@ -16,4 +25,8 @@ class ThreadedComponentClass(ABC):
         ...
 
     def start_loop_thread(self) -> None:
+        self.run = True
         start_new_thread(self.main_loop, ())
+
+    def stop_loop_thread(self) -> None:
+        self.run = False
